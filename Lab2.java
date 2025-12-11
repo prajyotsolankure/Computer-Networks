4850d99b15a946e896295a45ef51b04708b17bd4
prajyotsolankure <prajyotsolankureof@gmail.com>
2025-12-11 09:06:23 +0530

    2

--- Lab2.java
+++ Lab2.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.io.*;
 
 public class Labman {
     public static void main(String args[]) {
@@ -15,28 +14,44 @@ public class Labman {
         int data[] = new int[message.length() + generator.length() - 1];
         int divisor[] = new int[generator.length()];
 
+        // Copy message bits into data[]
         for (int i = 0; i < message.length(); i++)
-            data[i] = Integer.parseInt(message.charAt(i) + "");
+            data[i] = message.charAt(i) - '0';
 
+        // Append zeros for CRC (already 0 by default)
+
+        // Copy generator into divisor[]
         for (int i = 0; i < generator.length(); i++)
-            divisor[i] = Integer.parseInt(generator.charAt(i) + "");
+            divisor[i] = generator.charAt(i) - '0';
 
-        // Calculation of CRC
+        // CRC Calculation
         for (int i = 0; i < message.length(); i++) {
-            if (data[i] == 1)
+            if (data[i] == 1) {
                 for (int j = 0; j < divisor.length; j++)
                     data[i + j] ^= divisor[j];
+            }
         }
 
-        // Display Checksum
-        System.out.print("The checksum code is: ");
+        // Create final codeword: original message + remainder
+        int codeword[] = new int[message.length() + generator.length() - 1];
+
+        // Copy original message
         for (int i = 0; i < message.length(); i++)
-            data[i] = Integer.parseInt(message.charAt(i) + "");
-        for (int i = 0; i < data.length; i++)
-            System.out.print(data[i]);
+            codeword[i] = message.charAt(i) - '0';
+
+        // Copy remainder from data[]
+        for (int i = message.length(); i < codeword.length; i++)
+            codeword[i] = data[i];
+
+        // Print checksum/codeword
+        System.out.print("The checksum code is: ");
+        for (int i = 0; i < codeword.length; i++)
+            System.out.print(codeword[i]);
         System.out.println();
 
-        // Check for Input CRC Code
+
+        // ---------------- Receiver Side -----------------
+
         System.out.print("Enter checksum code: ");
         message = sc.nextLine();
 
@@ -46,20 +61,22 @@ public class Labman {
         data = new int[message.length() + generator.length() - 1];
         divisor = new int[generator.length()];
 
+        // Copy received message to data[]
         for (int i = 0; i < message.length(); i++)
-            data[i] = Integer.parseInt(message.charAt(i) + "");
+            data[i] = message.charAt(i) - '0';
 
+        // Copy generator
         for (int i = 0; i < generator.length(); i++)
-            divisor[i] = Integer.parseInt(generator.charAt(i) + "");
+            divisor[i] = generator.charAt(i) - '0';
 
-        // Calculation of Remainder
+        // CRC Remainder Calculation
         for (int i = 0; i < message.length(); i++) {
             if (data[i] == 1)
                 for (int j = 0; j < divisor.length; j++)
                     data[i + j] ^= divisor[j];
         }
 
-        // Display Validity of Data
+        // Check if remainder is all zeros
         boolean valid = true;
         for (int i = 0; i < data.length; i++) {
             if (data[i] == 1) {
